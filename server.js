b15e84c74e123e50a724f7fe00edd4e99613604c
Sergii <s.trotsenko@gmail.com>
2018-05-23 18:45:01 +0200

    heroku integration

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/www'));
+
+app.get('*', function (request, response) {
+    response.redirect('/');
+});
+
+app.listen(process.env.PORT || 8080);
